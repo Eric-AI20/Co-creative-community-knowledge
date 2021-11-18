844c3e80bd8494c263a0617e189997c375e98936
c21106601 <ahirea@cardiff.ac.uk>
2021-11-18 22:50:22 +0000

    initial commit

---
+++ src/main/java/com/cardiff/community/engagement/CommunityEngagementApplication.java
@@ -0,0 +1,13 @@
+package com.cardiff.community.engagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CommunityEngagementApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CommunityEngagementApplication.class, args);
+    }
+
+}
