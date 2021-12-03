3c854e0bfc1b726f425442c5f159d191f22698fc
c1734815 <eleftheiadist@cardiff.ac.uk>
2021-12-03 12:14:47 +0000

    Merge branch 'develop' into feature/viewCommunityList
    # Conflicts:
    #	src/main/resources/templates/home.html

--- src/main/java/com/cardiff/controller/CommunityController.java
+++ src/main/java/com/cardiff/controller/CommunityController.java
@@ -1,14 +1,24 @@
 package com.cardiff.controller;
 
+import com.cardiff.domain.UserDto;
 import com.cardiff.entity.Community;
+import com.cardiff.entity.User;
+import com.cardiff.exception.UserAlreadyExistException;
 import com.cardiff.service.CommunityService;
 import com.cardiff.service.FragmentService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.validation.Valid;
 
 @Controller
 public class CommunityController {
@@ -24,6 +34,30 @@ public class CommunityController {
     @Autowired
     public void setCommunityService(CommunityService communityService) {
         this.communityService = communityService;
+    }
+
+//    @PostMapping("/home/registration")
+//    public ModelAndView registerUserAccount(@ModelAttribute("community") @Valid UserDto userDto, HttpServletRequest request, Errors errors) {
+//        ModelAndView mav = new ModelAndView("registration");
+//        User registered = null;
+//        try {
+//            registered = community.registerNewUserAccount(userDto);
+//            mav.addObject("message",
+//                    "User registered successfully");
+//
+//        } catch (UserAlreadyExistException uaeEx) {
+//            mav.addObject("message", "An account for that username/email already exists.");
+//            return mav;
+//        }
+//        return new ModelAndView("successRegister", "user", userDto);
+//
+//    }
+//}
+
+    @GetMapping("/home/NewCommunity")
+    public String ShowRegistrationForm(WebRequest request, Model model) {
+        model.addAttribute("community", new Community());
+        return "NewCommunity";
     }
 
     @GetMapping("/home")
