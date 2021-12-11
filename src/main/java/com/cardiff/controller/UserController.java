8317a20b27a49f53d14a72fcc206120f0a39aa9c
Abhijeet <ahirea@cardiff.ac.uk>
2021-12-11 12:41:30 +0000

    #26 Created testcases for other stories and using h2 in-memory database for test

--- src/main/java/com/cardiff/controller/UserController.java
+++ src/main/java/com/cardiff/controller/UserController.java
@@ -46,7 +46,7 @@ public class UserController {
             mav.addObject("message", "An account for that username/email already exists.");
             return mav;
         }
-        return new ModelAndView("successRegister", "user", userDto);
+            return new ModelAndView("successRegister", "user", userDto);
 
     }
 }
