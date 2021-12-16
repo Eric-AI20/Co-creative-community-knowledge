fa7948f35b8cbc77d96ec599c4f8bc6bd24c1ef6
c21115786 <wellingtont@cardiff.ac.uk>
2021-12-16 02:15:45 +0000

    #34 Created ProjectRepositoryTest

--- src/test/resources/data-h2.sql
+++ src/test/resources/data-h2.sql
@@ -6,3 +6,11 @@ INSERT INTO communities
 VALUES (2, 'About US', 'description', 'Goal', 'History', 'Test1');
 
 
+INSERT INTO projects
+(id, about, description, objectives, author, url, address, name)
+VALUES (1, 'About the Author', 'Description', 'objectives', 'Author', 'Url', 'address', 'Test');
+INSERT INTO projects
+(id, about, description, objectives, author, url, address, name)
+VALUES (2, 'About the Author', 'Description', 'Objectives', 'Author', 'Url', 'address', 'Test1');
+
+
