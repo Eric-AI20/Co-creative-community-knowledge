e80cb1775169c228e639a57d569f9bede974a8c5
c21083456 <AiY1@cardiff.ac.uk>
2022-01-13 03:57:45 +0000

    added some comments in Case Study parts

--- src/main/java/com/cardiff/controller/CaseStudyController.java
+++ src/main/java/com/cardiff/controller/CaseStudyController.java
@@ -33,48 +33,52 @@ public class CaseStudyController {
     public void setCaseStudyService(CaseStudyService caseStudyService) {
         this.caseStudyService = caseStudyService;
     }
-
+    //display the existing case study
     @GetMapping("/viewCaseStudy/{id}")//URL
     public String showCaseStudyPage(WebRequest request, Model model, @PathVariable Long id) {
-
-        //model.addAttribute("user", new UserDto());
+        /*
+            transfer attributes from database to HTML to display
+         */
         model.addAttribute("casestudy", caseStudyService.findById(id));
         model.addAttribute("communityList", fragmentService.getAllCommunitiesForNavigation());
         return "ViewCaseStudy";//Return to the HTML page
     }
 
+    //see the case study the user just created
     @GetMapping("/createCaseStudy/{id}")//URL
     public String showCreateCaseStudyPage(WebRequest request, Model model, @PathVariable Long id) {
         CaseStudy caseStudy = new CaseStudy();
         Community community = new Community();
         community.setId(id);
         caseStudy.setCommunity(community);
-
+/*
+    add attribute from case study table and send to the front end
+ */
         model.addAttribute("casestudy", caseStudy);
         model.addAttribute("communityList", fragmentService.getAllCommunitiesForNavigation());
-        return "createCaseStudy";//Return to the HTML page
+        return "createCaseStudy";
     }
 
     @PostMapping("/createCaseStudy")
     public ModelAndView saveCaseStudyForm(@ModelAttribute("caseStudy") CaseStudy caseStudy, HttpServletRequest request) {
+        //create a object mav
         ModelAndView mav = new ModelAndView("redirect:/viewCaseStudy/");
-
+        //try catch
         try {
-            Community community = new Community();
-            community.setId(caseStudy.getCommunityId());
+            Community community = new Community();//new community object
+            community.setId(caseStudy.getCommunityId());//set the id to make sure the id in community table is equal to the community id in case study table
             caseStudy.setCommunity(community);
             CaseStudy savedCaseStudy = caseStudyService.saveCaseStudy(caseStudy);
             mav.addObject("message",
                     "CaseStudy create successfully");
             Long caseStudyId = savedCaseStudy.getId();
             mav.setViewName(mav.getViewName() + caseStudyId);
-        } catch (ExpressionException e) {
+        } catch (ExpressionException e) {//report system error
             mav.addObject("message", "error");
             return mav;
         }
 
         return mav;
-
 
     }
 
