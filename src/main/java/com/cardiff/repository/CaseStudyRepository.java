6725b4b59207dbc62922abc7579c989608dd5a15
Yihang Ai <aiy1@cardiff.ac.uk>
2022-01-14 08:05:29 +0000

    added some comments

--- src/main/java/com/cardiff/repository/CaseStudyRepository.java
+++ src/main/java/com/cardiff/repository/CaseStudyRepository.java
@@ -1,7 +1,10 @@
 package com.cardiff.repository;
-
+/*
+    case study repository
+    interface for JPA
+    keep connecting with database table
+ */
 import com.cardiff.entity.CaseStudy;
-
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
