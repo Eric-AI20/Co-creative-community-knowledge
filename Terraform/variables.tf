4872e2e9ebfb8433a0891943912776ce39c70256
Abhijeet <ahirea@cardiff.ac.uk>
2022-04-05 15:38:32 +0100

    Updated Terraform files

--- Terraform/variables.tf
+++ Terraform/variables.tf
@@ -1,11 +1,12 @@
 variable "flavor" { default = "m1.large" }
-variable "image" { default = "CentOS 8 20200402" }
-variable "name1" { default = "CentOS Takeaway from Jenkins" }
+variable "image" { default = "CentOS 8 Stream 20210603" }
+variable "name1" { default = "CommunityEngagement Centos" }
 
-variable "keypair" { default = "tmp" } # you may need to change this
-variable "network" { default = "ianNetwork" }   # you need to change this
+    variable "network" { default = "21106601_network" }   # you need to change this
+
+    variable "keypair" { default = "21106601_key_new" } # you need to change this
 
 variable "pool" { default = "cscloud_private_floating" }
 variable "server1_script" { default = "./server1.sh" }
 variable "security_description" { default = "Terraform security group" }
-variable "security_name" { default = "tf_security_takeaway" }
+variable "security_name" { default = "tf_securityMat" }
