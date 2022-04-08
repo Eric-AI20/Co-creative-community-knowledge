0bebcf169cbd263024f220e29d530f7377448698
Abhijeet <ahirea@cardiff.ac.uk>
2022-04-08 15:16:38 +0100

    Made changes for Debian OS

--- Terraform/server1.sh
+++ Terraform/server1.sh
@@ -13,7 +13,7 @@ sudo systemctl start mariadb
 sudo systemctl status mariadb
 sudo systemctl enable mariadb
 
-myql --user=root <<_EOF_
+sudo mysql --user=root <<_EOF_
 UPDATE mysql.user SET Password=PASSWORD('comsc') WHERE User='root';
 DELETE FROM mysql.user WHERE User='';
 DELETE FROM mysql.user WHERE User='root' AND Host NOT IN ('localhost', '127.0.0.1', '::1');
