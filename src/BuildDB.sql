799bb3b90f1f2d0906b678b0000feefa75b4f25b
Peiyu Xiao <xiaop@cardiff.ac.uk>
2022-04-21 13:29:19 +0000

    Update BuildDB.sql

--- src/BuildDB.sql
+++ src/BuildDB.sql
@@ -1,1 +1,13 @@
 CREATE DATABASE IF NOT EXISTS db_co_create;
+use db_co_create;
+drop table if exists items;
+
+create table if not exists items(
+    id int auto_increment primary key,
+    name varchar (25),
+    description varchar (100),
+    price int
+);
+
+
+insert into items(name, description, price) values ("Chips", "Chippy", 150);
