1cc2fb7954f71182fffef3ba988e0504e59be074
Abhijeet <ahirea@cardiff.ac.uk>
2022-04-24 21:10:30 +0100

    Disabled Chrome driver test for Jenkins build

--- src/test/java/com/cardiff/controller/CommunityControllerChromeDriverTest.java
+++ src/test/java/com/cardiff/controller/CommunityControllerChromeDriverTest.java
@@ -1,8 +1,9 @@
 package com.cardiff.controller;
 
+import org.junit.Ignore;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
-import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
@@ -12,36 +13,35 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 import static org.junit.Assert.assertTrue;
 
-//@RunWith(SpringRunner.class)
-//@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
-//public class CommunityControllerChromeDriverTest {
-//
-//    private WebDriver webDriver;
-//
-//    @Value("${local.server.port}")
-//    private int port;
-//
-//
-//
-//    @Test
-//    public void testingPageContents() {
-//        assertTrue(true);
-////
-////
-////        System.setProperty("webdriver.chrome.driver", "C:\\Users\\c21106601\\OneDrive - Cardiff University\\Documents\\Tools\\chromedriver_win32\\chromedriver.exe");
-////        ChromeOptions options = new ChromeOptions();
-////        options.addArguments("--remote-debugging-port=42227");
-////      //  options.addArguments("--headless");
-////        this.webDriver = new ChromeDriver(options);
-////
-////
-////        this.webDriver.get("http://localhost:" + Integer.toString(port) + "/home");
-////
-////
-////
-////
-////
-////        webDriver.quit();
-//    }
-//
-//}
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+public class CommunityControllerChromeDriverTest {
+
+    private WebDriver webDriver;
+
+    @Value("${local.server.port}")
+    private int port;
+
+
+    @Disabled
+    @Test
+    public void testingPageContents() {
+        assertTrue(true);
+
+
+        System.setProperty("webdriver.chrome.driver", "C:\\Users\\c21106601\\Downloads\\chromedriver_win32 (1)\\chromedriver.exe");
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--remote-debugging-port=42227");
+        //  options.addArguments("--headless");
+        this.webDriver = new ChromeDriver(options);
+
+
+        this.webDriver.get("http://localhost:" + port + "/home");
+
+        System.out.println("hello world");
+
+        webDriver.quit();
+    }
+
+}
