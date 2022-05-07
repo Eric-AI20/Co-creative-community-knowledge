49a38c9ef950b65842a92284dbd8c537923fd005
c21083456 <AiY1@cardiff.ac.uk>
2022-05-07 19:40:51 +0100

    change keys and new instance name

--- Terraform/variables.tf
+++ Terraform/variables.tf
@@ -1,8 +1,8 @@
 variable "flavor" { default = "m1.large" }
 variable "image" { default = "Debian Buster 10.11.1 20211029" }
-variable "name1" { default = "Debain-Jenkins" }
+variable "name1" { default = "CommunityApplication_Debian" }
 
-variable "keypair" { default = "scmimc_os_keypair" } # you may need to change this
+variable "keypair" { default = "gitlab_project_keypair" } # you may need to change this
 variable "network" { default = "yihangNetwork" }   # you need to change this
 
 variable "pool" { default = "cscloud_private_floating" }
